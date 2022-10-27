dadb7f3e6c7ef95735b92bed4ba0a54b9f8ad52c
Louis Dev <59470478+LuoisDev@users.noreply.github.com>
2022-10-27 18:03:18 +0700

    check

---
+++ src/phong.js
@@ -0,0 +1,12 @@
+/* eslint-disable react/react-in-jsx-scope */
+/* eslint-disable react/display-name */
+import Editor from "./index";
+export default () => {
+  return (
+    <div>
+      <div style={{ padding: 10 }}>
+        <Editor id="asd" />
+      </div>{" "}
+    </div>
+  );
+};
